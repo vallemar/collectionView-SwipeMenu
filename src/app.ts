207b19d6f7990a2ae585d89e11016ad1d280ad83
Juan de Dios Martínez Vallejo <vallemardeveloper@gmail.com>
2023-05-15 20:11:33 +0200

    init

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,6 @@
 import { createApp } from 'nativescript-vue';
 import Home from './components/Home.vue';
+import  CollectionView  from '@nativescript-community/ui-collectionview/vue3';
+import  SwipeMenuPlugin  from '@nativescript-community/ui-collectionview-swipemenu/vue3';
 
-createApp(Home).start();
+createApp(Home).use(CollectionView).use(SwipeMenuPlugin).start();
